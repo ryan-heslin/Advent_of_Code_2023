6d6b16d57ad530e29b03bf44a067e06bd793bd9f
ryan-heslin <rwheslin@gmail.com>
2024-02-02 14:35:00 -0500

    See you in December!

--- solutions/day25.py
+++ solutions/day25.py
@@ -1,7 +1,8 @@
-from collections import defaultdict, deque
-from copy import deepcopy
-from itertools import combinations
+from collections import defaultdict
+from math import inf
+
 from utils.utils import split_lines
+
 
 def parse(lines):
     result = defaultdict(set)
@@ -14,55 +15,29 @@ def parse(lines):
             result[dest].add(source)
     return result
 
-def find_cc(graph):
-    n_components =  0
-    nodes = set(graph.keys())
-    sizes = []
 
-    while nodes:
-        size = 1
-        n_components += 1
-        start = nodes.pop()
-        queue = deque([start])
+# Algorithm "borrowed" from https://www.reddit.com/r/adventofcode/comments/18qbsxs/2023_day_25_solutions/
+def partition(graph):
+    S = set(graph.keys())
+    n = len(S)
 
-        while queue:
-            current = queue.pop()
-            for neighbor in graph[current]:
-                # Not yet explored, so remove
-                if neighbor in nodes:
-                    queue.append(neighbor)
-                    nodes.remove(neighbor)
-                    size += 1
-        sizes.append(size)
-    
-    if n_components == 2:
-        return sizes[0] * sizes[1]
+    while True:
+        most = -inf
+        target = None
+        all_edges = set()
+        for node in S:
+            in_other = {n for n in graph[node] if n not in S}
+            found = len(in_other)
+            if found > most:
+                target = node
+                most = found
+            all_edges |= {tuple(sorted((node, dest))) for dest in in_other}
+        if len(all_edges) == 3:
+            return len(S) * (n - len(S))
+        S.remove(target)
 
-def get_edges(graph):
-    return {tuple(sorted((source, dest))) for source, dests in graph.items() for dest in dests }
-
-def remove_edges(graph, edges):
-    new = deepcopy(graph)
-    for source, dests in graph.items():
-        for dest in dests:
-            if tuple(sorted((source, dest))) in edges:
-                new[source].discard(dest)
-                new[dest].discard(source)
-    return new
-
-def solve(graph, edges):
-    i = 0
-    for group in combinations(edges, r = 3):
-        i += 1
-        new_graph = remove_edges(graph, group)
-        result = find_cc(new_graph)
-        if result is not None:
-            return result
-        if i == 1000:
-            print(i)
 
 raw_input = split_lines("inputs/day25.txt")
 graph = parse(raw_input)
-edges = get_edges(graph)
-part1 = solve(graph, edges)
+part1 = partition(graph)
 print(part1)
