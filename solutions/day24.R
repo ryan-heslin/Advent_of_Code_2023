e5c17acb82da9a985afa2a9b00c5af0ecce54c1a
ryan-heslin <rwheslin@gmail.com>
2024-05-10 13:02:49 -0400

    Add rational class

--- solutions/day24.R
+++ solutions/day24.R
@@ -1,3 +1,160 @@
+setClass("Rational", 
+         slots = c(numerators = "integer", 
+         denominators = "integer", dim = "integer"), 
+         prototype = list(numerators  = 0L, 
+         denominators = 1L,  dim = 1L)
+)
+setValidity("Rational", function(object){
+    num_length <- if(is.matrix(object@numerators)) dim(object@numerators) else length(object@numerators)
+    denom_length <- if(is.matrix(object@denominators)) dim(object@denominators) else length(object@denominators)
+    if(identical(num_length, 0L) || identical(denom_length, 0L)){ 
+        "Numerator and denominator must have length at least 1"
+} else if(!identical(num_length, denom_length) && !(identical(num_length, 1L) || identical(denom_length, 1L) )){
+        "Numerators and denominators must have the same length"
+    
+    }else if(any(object@denominators == 0)){ 
+        "Denominators cannot contain 0"
+    } else{
+        TRUE
+    }
+})
+
+Rational <- function(numerators, denominators){ 
+num_length <- if(is.matrix(numerators)) dim(numerators) else length(numerators)
+denom_length <- if(is.matrix(denominators)) dim(denominators) else length(denominators)
+
+    # Recycle unit numerator or denominator
+    if(identical(num_length, 1L) && !identical(num_length , denom_length)){ 
+        numerators <- array(numerators, dim = denom_length)
+    }else if (identical(denom_length, 1L) && !identical(denom_length , num_length)){ 
+        denominators <- array(denominators, dim = num_length)
+    }
+    names(numerators) <- NULL
+    names(denominators) <- NULL
+    dims <- if(is.matrix(denominators)) dim(denominators) else length(denominators)
+    new("Rational", numerators = numerators, denominators = denominators, dim = dims)
+}
+
+ #setGeneric("dim", function(x) standardGeneric("dim"))
+setGeneric("numerators", function(x) standardGeneric("numerators"))
+setGeneric("denominators", function(x) standardGeneric("denominators"))
+setGeneric("numerators<-", function(x, ...) standardGeneric("numerators<-"), signature = "x")
+setGeneric("denominators<-", function(x, ...) standardGeneric("denominators<-"), signature = "x")
+setGeneric("simplify", function(x) standardGeneric("simplify"))
+
+setMethod("numerators", "Rational", function(x) x@numerators)
+setMethod("numerators<-", "Rational", function(x, value) Rational(x@numerators, value))
+setMethod("denominators", "Rational", function(x) x@denominators)
+setMethod("denominators<-", "Rational", function(x, value) Rational(x@numerators, value))
+setMethod("dim", "Rational", function(x) x@dim  )
+setMethod("simplify", "Rational", function(x){ 
+    gcds <- mapply(numerators(x), denominators(x), FUN = gcd, SIMPLIFY = FALSE)
+    x@numerators <- x@numerators %/% gcds
+    x@denominators <- x@denominators %/% gcds
+    validObject(x)
+    x
+})
+
+
+ setMethod("show", "Rational", function(object){ 
+    dims <- dim(object)
+    left <- as.character(object@numerators)
+    right <- as.character(object@denominators)
+    paste0(left, "/", right)
+    
+})
+
+gcd <- function(a, b){
+    while (b != 0){
+        t  <- b
+        b <- a %% b
+        a <- t
+    }
+    a
+}
+
+simplify_matrix <- function(A){
+    divisors <- matrix(vapply(A, \(x) gcd(Re(x), Im(x)), FUN.VALUE = numeric(1)), nrow = nrow(A))
+    A * (1 / matrix(divisors, nrow = nrow(A)))
+}
+
+frac_divide <- function(num, denom){
+    complex(real = Re(num) * Im(denom), imaginary = Im(num) * Re(denom))
+}
+
+frac_multiply <- function(x, y){ 
+    complex(real = Re(x) * Re(y), imaginary = Im(x) * Im(y))
+}
+
+frac_subtract <- function(x, y){ 
+    complex(real = (Re(x) * Im(y)) - ((Im(x) * Re(y))), imaginary = Im(x) * Im(y))
+}
+frac_add <- function(x, y){ 
+complex(real = (Re(x) * Im(y)) + ((Im(x) * Re(y))), imaginary = Im(x) * Im(y))
+}
+
+rref <- function(A){
+    pivot <- 1
+    n <- nrow(A)
+    m <- ncol(A)
+    numerators <- A 
+    # Real is numerator, imag is denominator
+    result <- A + 1i
+    denoms <- matrix(1, nrow = n, ncol = m)
+
+    # TODO reorder by leading ones
+    for(pivot in seq(1, n)){ 
+        # Scale to 1
+        result[pivot, pivot:m] <- frac_divide(result[pivot, pivot:m], result[pivot, pivot])
+        # Subtract from lower row
+        current <- result[pivot, seq(pivot, m)]
+        # Clear leading values below
+        if (pivot < n){
+        for (below in seq(pivot + 1, n)){ 
+            factor <- result[below, pivot]
+            result[below, seq(pivot, m)]  <-
+                frac_subtract(result[below, seq(pivot, m)], frac_multiply(factor, current))
+
+    }
+        }
+}
+result
+}
+
+back_solve <- function(A){
+    n <- nrow(A)
+    m <- ncol(A)
+    col <- m -1
+
+    solution <- rep(0+1i, n)
+    for (row in rev(seq_len(n))){ 
+       rhs  <-  A[row ,m] 
+       if (col != m- 1){ 
+           rhs <-  frac_subtract(rhs, Reduce(x = frac_multiply(A[row, (col + 1):(m-1)], solution[(row +1) :n]), f=  frac_add))
+       }
+       solution[[row]] <- frac_divide(rhs, A[row, col])
+       col <- col - 1
+    }
+    solution
+}
+
+my_solve <- function(A){ 
+    result <- back_solve(rref(A))
+    Re(result) / Im(result)
+}
+
+test <- function(A, b){
+    stopifnot(all.equal(my_solve(cbind(A, b)), solve(A, b)))
+}
+
+for (i in seq(0, 100)){ 
+    A <- matrix(sample(seq(0, 100000), replace = FALSE, size = 12), nrow = 3)
+    test(A[,-4], A[,4])
+}
+
+
+
+
 find_intersection <- function(pair, low, high) {
     A_pos <- pair[[1]][[1]][-3]
     A_vel <- pair[[1]][[2]][-3]
@@ -54,6 +211,7 @@ make_row <- function(pair, first, second) {
 
 find_coord <- function(pairs, first, second) {
     equations <- c()
+    # Try until we find a coordinate pair where equation can be solved
     for (pair in pairs) {
         row <- make_row(pair, first, second)
         new <- rbind(equations, row)
@@ -64,8 +222,12 @@ find_coord <- function(pairs, first, second) {
     }
     # Solve for X, Y, ignore velocities
     result <- solve(equations[, -5], equations[, 5])
-    print(result)
-    result[1:2]
+    reduced <- simplify_matrix(rref(equations))
+    # My complex number hack for rational numbers
+    solution <- back_solve(reduced)
+    Re(solution[1:2]) / Im(solution[1:2])
+    # print(result)
+    # result[1:2]
 }
 
 solve_part2 <- function(pairs) {
@@ -85,4 +247,4 @@ part1 <- lapply(pairs, find_intersection, low = lower, high = upper) |>
 print(part1)
 
 part2 <- solve_part2(pairs)
-print(sum(part2))
+cat(as.character(sum(part2)), "\n")
